580532c9e379b89317c5e257203c920b898adb4f
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-25 18:12:10 +0100

    progress

--- P0/Task1.py
+++ P0/Task1.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -11,10 +12,24 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+import numpy as np
+
+
+def number_unique(text, call):
+    phone_numbers = []
+    for record in text:
+        phone_numbers.append(record[0])
+        phone_numbers.append(record[1])
+    for record in call:
+        phone_numbers.append(record[0])
+        phone_numbers.append(record[1])
+    return len(np.unique(phone_numbers))
+
+
+print("There are", number_unique(texts, calls), "different telephone numbers in the records.")
