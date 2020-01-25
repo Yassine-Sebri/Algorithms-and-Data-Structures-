2177bc2c8f7af988db111237eb35e7b2eea7b3f0
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-25 21:28:15 +0100

    progress

--- Part 2/Arrays and Linked Lists/Strings Exercice.py
+++ Part 2/Arrays and Linked Lists/Strings Exercice.py
@@ -21,3 +21,37 @@ def string_reverser(our_string):
 print("Pass" if ('retaw' == string_reverser('water')) else "Fail")
 print("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
 print("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
+
+
+def anagram_checker(str1, str2):
+    """
+    Check if the input strings are anagrams of each other
+
+    Args:
+       str1(string),str2(string): Strings to be checked
+    Returns:
+       bool: Indicates whether strings are anagrams
+    """
+
+    str1 = str1.lower().replace(" ","")
+    str2 = str2.lower().replace(" ","")
+    if len(str1) != len(str2):
+        return False
+
+    for char in str1:
+        if char not in str2:
+            return False
+        else:
+            str2 = str2.replace(char, "", 1)
+
+    return True
+
+
+# Test Cases
+
+print()
+print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
+print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
+print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
+print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
+print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")
