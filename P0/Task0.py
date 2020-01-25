32d06f8be494f44870f69fbafe009d7ce6cd86ea
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-25 16:11:51 +0100

    progress

--- P0/Task0.py
+++ P0/Task0.py
@@ -19,4 +19,4 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-
+print("First record of texts,", texts[0][0], "texts", texts[0][1], "at time", texts[0][2])
