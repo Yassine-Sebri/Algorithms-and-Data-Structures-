909b0ff2262012be6f88db099cdfb9508551343b
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-25 20:28:28 +0100

    progress

--- P0/Task4.py
+++ P0/Task4.py
@@ -25,3 +25,29 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+def telemarketers(call_records, text_records):
+    send_calls = []
+    receive_calls = []
+    send_texts = []
+    receive_texts = []
+    possible = []
+    for call_record in call_records:
+        if call_record[0] not in send_calls:
+            send_calls.append(call_record[0])
+        if call_record[1] not in receive_calls:
+            receive_calls.append(call_record[1])
+    for text_record in text_records:
+        if text_record[0] not in send_texts:
+            send_texts.append(text_record[0])
+        if text_record[1] not in receive_texts:
+            receive_texts.append(text_record[1])
+    for number in send_calls:
+        if number not in receive_calls and number not in send_texts and number not in receive_texts:
+            possible.append(number)
+    return possible
+
+
+print("These numbers could be telemarketers: ")
+for telemarketer in telemarketers(calls, texts):
+    print(telemarketer)
