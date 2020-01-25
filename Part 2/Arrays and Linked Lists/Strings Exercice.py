0b54de9bb55add1d4e0bb2f8c52e5178ef90653a
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-25 21:02:46 +0100

    progress

---
+++ Part 2/Arrays and Linked Lists/Strings Exercice.py
@@ -0,0 +1,23 @@
+def string_reverser(our_string):
+
+    """
+    Reverse the input string
+
+    Args:
+       our_string(string): String to be reversed
+    Returns:
+       string: The reversed string
+    """
+    index = len(our_string) - 1
+    reversed_string = ""
+    while index > -1:
+        reversed_string += our_string[index]
+        index -= 1
+    return reversed_string
+
+
+# Test Cases
+
+print("Pass" if ('retaw' == string_reverser('water')) else "Fail")
+print("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
+print("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
