3009e70a0d3b1b6c8bf68b0b016b321bb4111dbc
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-26 03:06:04 +0100

    progress

---
+++ Part 2/Recursion/List of Permutations.py
@@ -0,0 +1,73 @@
+import copy
+
+
+def permute(l):
+    """
+    Return a list of permutations
+
+    Examples:
+       permute([0, 1]) returns [ [0, 1], [1, 0] ]
+
+    Args:
+      l(list): list of items to be permuted
+
+    Returns:
+      list of permutation with each permuted item being represented by a list
+    """
+    if len(l) <= 1:
+        return [l]
+
+    elif len(l) == 2:
+        return [l, l[::-1]]
+
+    else:
+        output = []
+        current_level = l[0]
+        prev_level = permute(l[1:])
+
+        for element in prev_level:  # For each answer from previous level
+            for pos in range(len(element) + 1):  # For each possible position to put this level value
+                temp_list = []
+                temp_element = element.copy()
+                for i in range(len(element) + 1):
+                    if pos == i:
+                        temp_list.append(current_level)
+                    else:
+                        temp_list.append(temp_element.pop())
+                output.append(temp_list)
+        return output
+
+
+# Test Cases
+
+# Helper Function
+def check_output(output, expected_output):
+    """
+    Return True if output and expected_output
+    contains the same lists, False otherwise.
+
+    Note that the ordering of the list is not important.
+
+    Examples:
+        check_output([ [0, 1], [1, 0] ] ], [ [1, 0], [0, 1] ]) returns True
+
+    Args:
+        output(list): list of list
+        expected_output(list): list of list
+
+    Returns:
+        bool
+    """
+    o = copy.deepcopy(output)  # so that we don't mutate input
+    e = copy.deepcopy(expected_output)  # so that we don't mutate input
+
+    o.sort()
+    e.sort()
+    return o == e
+
+
+print("Pass" if (check_output(permute([]), [[]])) else "Fail")
+print("Pass" if (check_output(permute([0]), [[0]])) else "Fail")
+print("Pass" if (check_output(permute([0, 1]), [[0, 1], [1, 0]])) else "Fail")
+print("Pass" if (
+    check_output(permute([0, 1, 2]), [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]])) else "Fail")
